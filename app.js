2e9ca01ae92b8f77e1fdf945c2c037cfaf671131
kailevy <kai0levy@gmail.com>
2015-03-31 12:08:39 -0400

    Added images and favicons, slight reformatting

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var mongoose = require('mongoose');
+// var favicon = require('serve-favicon');
 var app = express();
 
 var index = require('./routes/index');
@@ -21,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(favicon(path.join(__dirname,'public/images/favicon.ico')));
 
 app.get('/', index.home);
 app.get('/sightings', sightings.sightings);
