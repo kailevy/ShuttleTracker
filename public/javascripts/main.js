0becb041566f5c52f882ef08790d88bb846759ed
kailevy <kai0levy@gmail.com>
2015-03-31 12:41:33 -0400

    Added cat gif functionality

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,6 +8,12 @@ var onSuccess = function(data, status) {
   $("#lastTime").html('<strong>'+data.timestamp+'</strong>');
   $("#changeHeader").html('Thank you for helping!');
   $("#hideForm").remove();
+  $("#cats").show();
+  $("#cats").click(function () {
+    var imgUrl = $(this).data('rel');
+    $("#area").html("<img style='max-width:20rem; height:auto;' src='" + imgUrl + "' alt='cats' />");
+    $("#cats").hide();
+  });
 };
 
 var onError = function(data, status) {
